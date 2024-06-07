f8f376c1b53e0fdad249c80fce10cca5226c1ec0
scorhenao <samcorhenao@gmail.com>
2024-06-07 19:56:35 -0400

    commits for crud

--- services/fetch.js
+++ services/fetch.js
@@ -1,8 +1,19 @@
+/**Logic of CRUD
+ * C -> Create : POST
+ * R -> Read : GET
+ * U -> Update : PUT/PATCH
+ * D -> Delete : DELETE
+ */
+
+/**Get method to get the information of an enpoint */
 export async function read(URL) {
     const data = await fetch(URL);
     return await data.json();
 }
 
+/**Post method to send new information throughout an enpoint
+ * The info that gonna be save in the api json
+ */
 export async function create(URL,data) {
     const response = await fetch(URL, {
         method: 'POST',
