42662ab11ec245f37c767d15f38c655da3504929
scorhenao <samcorhenao@gmail.com>
2024-06-07 18:49:36 -0400

    fetch services

---
+++ services/fetch.js
@@ -0,0 +1,16 @@
+export async function read(URL) {
+    const data = await fetch(URL);
+    return await data.json();
+}
+
+export async function create(URL,data) {
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+}
